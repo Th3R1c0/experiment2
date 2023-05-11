adccaeeeb7889850515c1678972de8242b43a69f
Th3R1c0 <ricots114@protonmail.com>
2023-05-11 16:46:43 +1200

    commit

---
+++ src/components/EarthScene.tsx
@@ -0,0 +1,21 @@
+const EarthScene = () => {
+  return (
+    <div className="flex h-screen w-full bg-black">
+      <div className="flex  flex-col justify-left items-center p-48 w-full h-full bg-black">
+        <h3 className="text-4xl font-bold tracking-wide">
+          Based in New zealand
+        </h3>
+        <p>
+          We develop future ready solutions in the latest technology. It’s what
+          we do. It’s who we are. It’s why we can deliver. Every single time. If
+          you’re looking to put your customer at the heart of your business,
+          you’re in the right place. To top it all off we have the right tools
+          for technical SEO, performance, and accessibility. Making your website
+          purr like a fast, smooth thing. What are you waiting for? Let’s chat.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default EarthScene;
